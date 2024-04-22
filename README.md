d472583f81eff17d26b257cec6f3957216baa06c
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-04-22 18:07:42 +0000

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# egiftcard-v2-core
+# Egiftcard V2
+
+[![Actions Status](https://github.com/Egiftcard/egiftcard-v2-core/workflows/CI/badge.svg)](https://github.com/Egiftcard/egiftcard-v2-core/actions)
+[![Version](https://img.shields.io/npm/v/@egiftcard/v2-core)](https://www.npmjs.com/package/@egiftcard/v2-core)
+
+In-depth documentation on Egiftcard V2 is available at [egiftcard.cc](https://egiftcard.cc/docs).
+
+The built contract artifacts can be browsed via [unpkg.com](https://unpkg.com/browse/@egiftcard/v2-core@latest/).
+
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+`yarn compile`
+
+## Run Tests
+
+`yarn test`
---
+++ contracts/EgiftcardV2Factory.sol
@@ -0,0 +1,49 @@
+pragma solidity =0.5.16;
+
+import './interfaces/IEgiftcardV2Factory.sol';
+import './EgiftcardV2Pair.sol';
+
+contract EgiftcardV2Factory is IEgiftcardV2Factory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'EgiftcardV2: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'EgiftcardV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'EgiftcardV2: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(EgiftcardV2Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IEgiftcardV2Pair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'EgiftcardV2: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'EgiftcardV2: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
